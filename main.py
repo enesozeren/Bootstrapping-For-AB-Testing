d12483cc7762de40c613953fe274ad33c188383a
Enes Özeren <enesozeren7@gmail.com>
2022-10-09 14:48:56 +0300

    Other subheaders are added to study in the near feature

--- main.py
+++ main.py
@@ -18,9 +18,16 @@ st.caption('Total IGT (in minutes) of players in the test time period (Only 10 d
 # Summary of the Data
 st.subheader('Summary of the Data')
 func.summary_of_data(data)
-
 st.write('We can use bootstrapping method to calculate the statistical significance (P-Value).')
 
-# Bootstrap method
+# What is Bootstrapping?    
+st.subheader('Wait, what is bootstrapping?')
+st.write('TBD')
+
+# Implementing Bootstrap method
 st.subheader('P-Value Caclulation with Bootstrapping method')
 p_val = func.p_value_with_bootstrapping(data[data['Group']=='Control'], data[data['Group']=='Variant'], col_name='IGT')
+
+# Conclusion
+st.subheader('Conclusion')
+st.write('TBD')
